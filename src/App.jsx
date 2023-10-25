d1dfdefece7fc2cfc4dbfe457a19a4d0d1bf7a29
MherKK <Mherkevorkian2@gmail.com>
2023-10-25 14:36:36 +0400

    removed mysql db instead I added firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import './App.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Stylist, { stylistLoader } from './features/body/stylists/Stylist'
+import Stylist from './features/body/stylists/Stylist'
 import { useEffect } from 'react'
-import axios from "axios";
 import { setInitialState } from './features/body/home/ourteam/OurTeamSlice'
 import { useDispatch } from 'react-redux'
 import Services from './features/body/services/Services.jsx'
@@ -11,7 +10,6 @@ import Contact from './features/body/contact/Contact'
 import Home from './features/body/home/Home.jsx'
 import Stylists from './features/body/stylists/Stylists'
 import RootLayout from './RootLayout'
-import { useState } from 'react';
 import { dataRef } from './firebase';
 
 const router = createBrowserRouter(
@@ -19,7 +17,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />} >
       <Route index element={<Home />} ></Route>
       <Route path="/stylists" element={<Stylists />} />
-      <Route loader={stylistLoader} path='/stylists/:id' element={<Stylist />} />
+      <Route path='/stylists/:id' element={<Stylist />} />
       <Route path='/services' element={<Services />} />
       <Route path='/products' element={<Products />} />
       <Route path='/contact' element={<Contact />} />
@@ -31,12 +29,7 @@ const router = createBrowserRouter(
 function App() {
   const dispatch = useDispatch();
 
-  const [fire, setFire] = useState([]);
-
   useEffect(() => {
-    axios.get("http://localhost:5000/").then(res => {
-
-    })
 
     dataRef.ref().child('Stylers').on('value', data => {
       const getData = Object.values(data.val());
@@ -45,8 +38,6 @@ function App() {
       }))
     })
   }, [])
-
-  console.log(fire);
 
   return (
     <>
--- src/features/body/stylists/Stylist.jsx
+++ src/features/body/stylists/Stylist.jsx
@@ -1,26 +1,37 @@
-import { useLoaderData } from "react-router-dom";
-import axios from "axios";
+import { useParams } from "react-router-dom";
 import Stylists from "./Stylists";
 import "../services/services.css";
+import { dataRef } from "../../../firebase";
+import { useEffect, useState } from "react";
 
 
 export default function Stylist() {
-    const stylerData = useLoaderData();
-    const styler = (stylerData[0]);
 
+    const { id } = useParams();
+    const [stylerInfo, setStylerInfo] = useState({});
 
-
+    useEffect(() => {
+        dataRef.ref().child('Stylers').on('value', data => {
+            const getData = Object.values(data.val());
+            getData.map(data => {
+                if (data.id == id) {
+                    setStylerInfo(getData[0])
+                }
+            })
+        })
+    }, [])
+    console.log(stylerInfo);
     return (
         <div>
             <div className="service-container">
                 <div className="styling">
 
-                    <img className="stylist-image" src={styler.image} />
+                    <img className="stylist-image" src={stylerInfo.image} />
 
                     <div>
-                        <h1 style={{ margin: 0 }}>{styler.name} {styler.lastName}</h1>
-                        <h5 style={{ margin: 0 }}>{styler.role}</h5>
-                        <p>{styler.description}</p>
+                        <h1 style={{ margin: 0 }}>{stylerInfo.name} {stylerInfo.lastName}</h1>
+                        <h5 style={{ margin: 0 }}>{stylerInfo.role}</h5>
+                        <p>{stylerInfo.description}</p>
                     </div>
 
                 </div>
@@ -32,11 +43,5 @@ export default function Stylist() {
 }
 
 
-export const stylistLoader = async ({ params }) => {
-    const { id } = params;
-    let response = await axios.get("http://localhost:5000/stylists/" + id);
-    return response.data;
-
-}
 
 
