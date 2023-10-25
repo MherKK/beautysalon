3032f07261e3024294fb09a4bec87215ccff4760
MherKK <Mherkevorkian2@gmail.com>
2023-10-25 05:05:37 +0400

    deleted env

--- server/db.js
+++ server/db.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require("cors");
-require('dotenv').config();
 const bodyParser = require("body-parser")
 const mysql = require('mysql');
 const app = express();
