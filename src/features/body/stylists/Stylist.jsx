b5235fc49c72c65debcb570657a5799a0d91a1f8
MherKK <Mherkevorkian2@gmail.com>
2023-10-26 00:34:33 +0400

    fixed stylist,jsx

--- src/features/body/stylists/Stylist.jsx
+++ src/features/body/stylists/Stylist.jsx
@@ -1,47 +1,39 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import Stylists from "./Stylists";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ourTeamList } from "../home/ourteam/OurTeamSlice";
 import "../services/services.css";
+import { dataRef } from "../../../firebase";
+
 
 export default function Stylist() {
     const ourTeam = useSelector(ourTeamList);
     const { name } = useParams();
     const [stylerInfo, setStylerInfo] = useState({});
     let fullName = name.split('-');
-    console.log(name);
+    console.log(fullName);
     useEffect(() => {
-
-        ourTeam.filter(data => {
-            if (data.name + data.lastName == fullName.join('')) {
-                console.log(data);
-                return setStylerInfo(data)
-            }
+        dataRef.ref('Stylers/' + fullName.join('')).on('value', (data) => {
+            return setStylerInfo(data.val());
         })
-
-    }, [ourTeam, stylerInfo, setStylerInfo])
+    }, [fullName[0]])
     return (
         <div>
             <div className="service-container">
                 <div className="styling">
-
                     <img className="stylist-image" src={stylerInfo.image} />
-
                     <div>
                         <h1 style={{ margin: 0 }}>{stylerInfo.name} {stylerInfo.lastName}</h1>
                         <h5 style={{ margin: 0 }}>{stylerInfo.role}</h5>
                         <p>{stylerInfo.Description}</p>
                     </div>
-
                 </div>
             </div>
             <Stylists />
         </div>
-
     )
 }
-
 
 
 
